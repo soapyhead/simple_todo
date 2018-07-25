b9f2e700c21dc3d5b1cbce4ed0bdc5a557950990
Ruslan Akhmedzyanov <rta@tigrio.ru>
2018-07-25 14:54:43 +0700

    update custom_user app

--- custom_user/serializers.py
+++ custom_user/serializers.py
@@ -1,19 +1,71 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import User
+from companies.models import Company
 
 
-class UserSerializer(serializers.ModelSerializer):
+class CreateUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
         validators=[UniqueValidator(User.objects.all())]
     )
-    company = serializers.CharField(required=True)
+    companies = serializers.ListField(
+        child=serializers.CharField(min_length=1, max_length=80),
+        write_only=True
+    )
     password = serializers.CharField(min_length=8, write_only=True)
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id', 'email', 'password', 'companies')
 
     def create(self, validated_data):
-        pass
+        user = User.objects.create_user_with_companies(
+            email=validated_data['email'].lower(),
+            password=validated_data['password'],
+            companies=validated_data['companies']
+        )
+        return user
+
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    auth_company = serializers.CharField(min_length=1, max_length=80,
+                                         required=True)
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'auth_company')
+
+    def validate(self, data):
+        user = User.objects.filter(email__iexact=data['email']).first()
+        if user:
+            if not user.companies.filter(name__iexact=data['auth_company']):
+                raise serializers.ValidationError(
+                    "Company does not exist or user doesn't have access rights"
+                )
+            return data
+        else:
+            raise serializers.ValidationError('Unknown user')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    companies = serializers.StringRelatedField(many=True)
+    auth_company = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'companies', 'auth_company')
+
+    def get_auth_company(self, obj):
+        request = self.context['request']
+        return request.session.get('company', None)
+
+    def create(self, validated_data):
+        user = User.objects.create_user_with_companies(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            companies=validated_data['companies']
+        )
+        return user
--- custom_user/views.py
+++ custom_user/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import (
+    UserSerializer, CreateUserSerializer, LoginUserSerializer
+)
+
+
+class UserView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return Response(self.get_serializer(request.user).data,
+                        status=status.HTTP_200_OK)
+
+
+class CreateUserView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = CreateUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                data = UserSerializer(user,
+                                      context=self.get_serializer_context()).data
+                return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUserView(GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            company = serializer.validated_data['auth_company']
+
+            user = authenticate(
+                email=email,
+                password=password
+            )
+            if user:
+                request.session['company'] = company
+                login(request, user)
+
+                token, created = Token.objects.get_or_create(user=user)
+
+                if not created:
+                    token.created = timezone.now()
+                    token.save()
+
+                data = {'key': token.key}
+                return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutUserView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
